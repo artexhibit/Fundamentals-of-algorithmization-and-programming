3ffc8262b22cc7b4bbb0627bf7234ba9cc585a97
artexhibit <vendabal@yandex.ru>
2023-05-28 21:14:37 +0300

    add a homework about  creating a slider gallery using JS, CSS and HTML

---
+++ JS/Фотогалерея-слайдер/photos.js
@@ -0,0 +1,25 @@
+/*
+**	Список фотографий
+*/
+var photos = [
+	// cats
+	"cat1.jpg",
+	"cat2.jpg",
+	"cat3.jpg",
+	"cat4.jpg",
+	"cat5.jpg",
+
+	// dogs
+	"dog1.jpg",
+	"dog2.jpg",
+	"dog3.jpg",
+	"dog4.jpg",
+	"dog5.jpg",
+
+	//wild
+	"wild1.jpg", 
+	"wild2.jpg", 
+	"wild3.jpg",
+	"wild4.jpg",
+	"wild5.jpg"
+];
