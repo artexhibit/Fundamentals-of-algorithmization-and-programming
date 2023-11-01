39f227c2dfbdc5c601f1d3390823315f60ef427c
artexhibit <vendabal@yandex.ru>
2023-11-01 21:02:52 +0300

    add college homework about React props

---
+++ JS/React/Props/README.md
@@ -0,0 +1,11 @@
+<img width="1000" src="https://raw.githubusercontent.com/artexhibit/Fundamentals-of-algorithmization-and-programming/main/JS/React/Props/images/Demo.png">
+
+[🇷🇺 Russian Version](./README-RUS.md)
+
+## About
+
+College homework to work with React props. Here I created a row of 3 cards and passed them data from separate js file, using props.
+
+## Props
+
+Props are arguments passed into React components. It is similar to HTML elements attributes.
---
+++ JS/React/Props/content.js
@@ -0,0 +1,44 @@
+const cardsContent = [
+    {
+        id: "1",
+        title: "CustomPopup",
+        subtitle: 'Моя реализация всплывающего уведомления, которое Apple использует в iOS. <br /> <br /> Я создал его для использования в пет-проектах, чтобы отображать информацию пользователю в нативном дизайне, но с раширенным функционалом: текст прокручивается, если он длиннее ширины попапа, может изменять отображаемые данные на ходу.',
+        tags: [
+            "UIKit",
+            "iOS",
+            "Storyboard",
+            "XIB"
+        ],
+        imgSrc: "./images/customPopup.png",
+        gitLink: "https://github.com/artexhibit/CustomPopup",
+    },
+    {
+        id: "2",
+        title: "CustomNumpad",
+        subtitle: "Моя реализация клавиатуры с цифрами и простыми вычислительными операциями для использования в пет-проекте. <br /> <br /> Я использую её в пет-проекте вместо стандартной цифровой клавиатуры Apple, чтобы пользователи могли получить дополнительную функциональность и красивый дизайн.",
+        tags: [
+            "UIKit",
+            "iOS",
+            "Storyboard",
+            "XIB",
+            "UIBezierPath",
+            "CAShapeLayer",
+        ],
+        imgSrc: "./images/customNumpad.png",
+        gitLink: "https://github.com/artexhibit/CustomNumpad",
+    },
+    {
+        id: "3",
+        title: "UIViewWithDatePicker",
+        subtitle: "Моя реализация UIView для переключения между опциями и UIDatePicker, с возможностью подтверждать или отменять действие с помощью кнопок. <br /> <br /> Результатом работы стало умение создавать собственные кастомные элементы интерфейса и использовать их в приложении.",
+        tags: [
+            "UIKit",
+            "iOS",
+            "Storyboard",
+            "XIB",
+            "UITapGestureRecognizer",
+        ],
+        imgSrc: "./images/customUIViewWithDatePicker.png",
+        gitLink: "https://github.com/artexhibit/CustomUIViewWithDatePicker",
+    },
+];
